1dfb993bdd32017ae6ccbb43871fd17e1fb7d7e0
LEO <LEO@DESKTOP-ER0GBFR>
2024-01-23 22:36:50 -0300

    Modificaciones

--- src/ejerciciopracticamatrices/EjercicioPracticaMatrices.java
+++ src/ejerciciopracticamatrices/EjercicioPracticaMatrices.java
@@ -1,20 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejerciciopracticamatrices;
 
-/**
- *
- * @author LEO
- */
+import java.util.Scanner;
+
 public class EjercicioPracticaMatrices {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        int vector[] = new int[15];
+
+        Scanner teclado = new Scanner(System.in);
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Ingrese un numero para el vector");
+            vector[i] = teclado.nextInt();
+        }
+
+        int contador = 0;
+        for (int i = 0; i < 15; i++) {
+            if (vector[i] == 3) {
+                contador = contador + 1;
+            }
+        }
+        System.out.println("La cantidad de 3 que ahi dentro del vector es: "+ contador);
     }
-    
 }
