448bc56d5cc132a83d514881b957eacc91f2ee47
LEO <LEO@DESKTOP-ER0GBFR>
2024-01-24 20:14:51 -0300

    Modificaciones

--- src/ejerciciopracticamatrices/EjercicioPracticaMatrices.java
+++ src/ejerciciopracticamatrices/EjercicioPracticaMatrices.java
@@ -6,6 +6,7 @@ public class EjercicioPracticaMatrices {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        /*
         int vector[] = new int[15];
 
         Scanner teclado = new Scanner(System.in);
@@ -21,5 +22,31 @@ public class EjercicioPracticaMatrices {
             }
         }
         System.out.println("La cantidad de 3 que ahi dentro del vector es: "+ contador);
+         */
+     
+        Double matriz[][] = new Double[4][4];
+        Double suma = 0.0;
+        Scanner teclado = new Scanner(System.in);
+
+        for (int f = 0; f < 4; f++) {
+            for (int c = 0; c < 3; c++) {
+
+                System.out.println("Ingre el alumno n°: " + f);
+                matriz[f][c] = teclado.nextDouble();
+                suma = suma + matriz[f][c];
+
+            }
+            matriz[f][3] = suma / 3;
+            suma = 0.0;
+        }
+
+        for (int f = 0; f < 4; f++) {
+            System.out.println("La notas del alumno N°: " + f);
+            for (int c = 0; c < 3; c++) {
+                System.out.println("Nota N° " + c + " " + matriz[f][c]);
+
+            }
+            System.out.println("El promedio de las notas es: " + matriz[f][3]);
+        }
     }
 }
