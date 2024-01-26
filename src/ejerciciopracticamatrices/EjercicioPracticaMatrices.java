38aef224262e6ea9436eb8016444f6d9e077bf86
LEO <LEO@DESKTOP-ER0GBFR>
2024-01-25 23:10:07 -0300

    Modificaciones

--- src/ejerciciopracticamatrices/EjercicioPracticaMatrices.java
+++ src/ejerciciopracticamatrices/EjercicioPracticaMatrices.java
@@ -47,7 +47,7 @@ public class EjercicioPracticaMatrices {
 
             }
             System.out.println("El promedio de las notas es: " + matriz[f][3]);
-        }*/
+        }
 
         String ciudades[] = new String[5];
         Double minimas[] = new Double[5];
@@ -87,6 +87,52 @@ public class EjercicioPracticaMatrices {
 
         System.out.println("La temperatura maxima es de: " + max);
         System.out.println("Se registro en la ciudad de: " + ciudades[posMax]);
+         */
 
+        int vuelos[][] = new int[6][3];
+        Scanner teclado = new Scanner(System.in);
+
+        for (int f = 0; f < 6; f++) {
+            for (int c = 0; c < 3; c++) {
+
+                System.out.println("Ingrese la cantidad de ASIENTOS para el destino: " + f + " horario: " + c);
+                vuelos[f][c] = teclado.nextInt();
+            }
+        }
+
+        //ventas de asientos
+        Scanner teclado2 = new Scanner(System.in);
+        String bandera = "";
+        int destino, horario, asientos;
+
+        while (!bandera.equalsIgnoreCase("finish")) {
+            System.out.println("Ingrese el N° de destino:");
+            destino = teclado2.nextInt();
+
+            System.out.println("Ingrese el horario de vuelo:");
+            horario = teclado2.nextInt();
+
+            System.out.println("Ingrese el N° de asientos");
+            asientos = teclado2.nextInt();
+
+            if (destino >= 0 && destino <= 2) {
+                if (horario >= 0 && horario <= 2) {
+                    if (vuelos[destino][horario] >= asientos) {
+                        System.out.println("Su reserva fue realizada con exito");
+                        vuelos[destino][horario] = vuelos[destino][horario] - asientos;
+                    } else {
+                        System.out.println("No hay asientos disponibles");
+                    }
+                } else {
+                    System.out.println("Horario no existe. Ingese entre 0 y 2");
+                }
+            } else {
+                System.out.println("Destino no existe. Ingrese entre 0 y 5");
+            }
+
+            System.out.println(
+                    "¿Desea seguir continuar reservando?. Ingrese finish para terminar o cualquier valor para seguir.");
+            bandera = teclado2.next();
+        }
     }
 }
